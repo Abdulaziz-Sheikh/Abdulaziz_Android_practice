8fc37e5a32e89622a75986b168ff4403bbad98f8
Abdulaziz-Sheikh <abdulaziz9921@gmail.com>
2023-12-02 13:43:56 -0500

    i tested debugger and it seems theres no connection being established

--- app/src/main/java/Algonquin/cst2335/MainActivity.java
+++ app/src/main/java/Algonquin/cst2335/MainActivity.java
@@ -13,7 +13,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import  Algonquin.cst2335.databinding.ActivityMainBinding;
+import Algonquin.cst2335.databinding.ActivityMainBinding;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,8 +24,8 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
-//error
 public class MainActivity extends AppCompatActivity {
+
     protected String cityName;
     RequestQueue queue = null;
     String apiKey = null;
@@ -35,78 +35,88 @@ public class MainActivity extends AppCompatActivity {
     double max = 0;
     double min = 0;
     int humidity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //This part goes at the top of the onCreate function:
+        // Initialize view binding
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
+        // Initialize Volley request queue
         queue = Volley.newRequestQueue(this);
-        ActivityMainBinding binding = ActivityMainBinding.inflate( getLayoutInflater() );
-        setContentView(binding.getRoot());
 
+        // Button click listener
         binding.forecastButton.setOnClickListener(click -> {
-        cityName = binding.cityTextField.getText().toString();
-        apiKey = "36b70149bbb1187da8fe770fd7435f2b";
-        String stringUrl = null;
+            cityName = binding.cityTextField.getText().toString();
+            apiKey = "36b70149bbb1187da8fe770fd7435f2b";
+            String stringUrl = null;
 
-        try {
-            stringUrl = "https://api.openweathermap.org/data/2.5/weather?q="
-            + URLEncoder.encode(cityName, "UTF-8")
-            + "&appid=" + apiKey + "&units=metric";
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        //this goes in the button click handler:
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, stringUrl, null,
-            (response) -> {
-        try {
-            JSONArray weatherArray = response.getJSONArray("weather");
-            JSONObject position0 = weatherArray.getJSONObject(0);
-            description = position0.getString("description");
-            iconName = position0.getString("icon");
-            JSONObject mainObject = response.getJSONObject("main");
-            current = mainObject.getDouble("temp");
-            min = mainObject.getDouble("temp_min");
-            max = mainObject.getDouble("temp_max");
-            humidity = mainObject.getInt("humidity");
-            String imageUrl = "https://openweathermap.org/img/w/"+ iconName + ".png";
-            ImageRequest imgReq = new ImageRequest(imageUrl, (bitmap) -> {
-                try {
-                    FileOutputStream fOut = openFileOutput( iconName + ".png", Context.MODE_PRIVATE);
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
-                    fOut.flush();
-                    fOut.close();
-                    runOnUiThread(() -> {
-                        binding.icon.setImageBitmap(bitmap);
-                        binding.icon.setVisibility(View.VISIBLE);
+            try {
+                stringUrl = "https://api.openweathermap.org/data/2.5/weather?q="
+                        + URLEncoder.encode(cityName, "UTF-8")
+                        + "&appid=" + apiKey + "&units=metric";
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            // Make JsonObjectRequest
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, stringUrl, null,
+                    response -> {
+                        try {
+                            JSONArray weatherArray = response.getJSONArray("weather");
+                            JSONObject position0 = weatherArray.getJSONObject(0);
+                            description = position0.getString("description");
+                            iconName = position0.getString("icon");
+                            JSONObject mainObject = response.getJSONObject("main");
+                            current = mainObject.getDouble("temp");
+                            min = mainObject.getDouble("temp_min");
+                            max = mainObject.getDouble("temp_max");
+                            humidity = mainObject.getInt("humidity");
+                            String imageUrl = "https://openweathermap.org/img/w/" + iconName + ".png";
+
+                            // Make ImageRequest
+                            ImageRequest imgReq = new ImageRequest(imageUrl, bitmap -> {
+                                try {
+                                    FileOutputStream fOut = openFileOutput(iconName + ".png", Context.MODE_PRIVATE);
+                                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                                    fOut.flush();
+                                    fOut.close();
+                                    runOnUiThread(() -> {
+                                        binding.icon.setImageBitmap(bitmap);
+                                        binding.icon.setVisibility(View.VISIBLE);
+                                    });
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+
+                            }, 1024, 1024, ImageView.ScaleType.CENTER, null,
+                                    error -> {
+                                    });
+                            queue.add(imgReq);
+
+                            // Update UI
+                            runOnUiThread(() -> {
+                                binding.temp.setText("The current temperature is " + current);
+                                binding.temp.setVisibility(View.VISIBLE);
+                                binding.maxTemp.setText("The max temperature is " + max);
+                                binding.maxTemp.setVisibility(View.VISIBLE);
+                                binding.minTemp.setText("The min temperature is " + min);
+                                binding.minTemp.setVisibility(View.VISIBLE);
+                                binding.humidity.setText("The humidity is " + humidity + "%");
+                                binding.humidity.setVisibility(View.VISIBLE);
+                                binding.description.setText(description);
+                                binding.description.setVisibility(View.VISIBLE);
+                            });
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    },
+                    error -> {
                     });
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-
-            }, 1024, 1024, ImageView.ScaleType.CENTER, null,
-            (error) -> {});
-            queue.add(imgReq);
-            runOnUiThread(() -> {
-                binding.temp.setText("The current temperature is " + current);
-                binding.temp.setVisibility(View.VISIBLE);
-                binding.maxTemp.setText("The max temperature is " + max);
-                binding.maxTemp.setVisibility(View.VISIBLE);
-                binding.minTemp.setText("The min temperature is " + min);
-                binding.minTemp.setVisibility(View.VISIBLE);
-                binding.humidity.setText("The humidity is " + humidity + "%");
-                binding.humidity.setVisibility(View.VISIBLE);
-                binding.description.setText(description);
-                binding.description.setVisibility(View.VISIBLE);
-            });
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    },
-        (error) -> {});
-        queue.add(request);
-    });
+            queue.add(request);
+        });
     }
-
 }
